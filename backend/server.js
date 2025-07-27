1d63e8a498a129c9410a2b1df3c447db9548c412
mguruprasath416 <153301868+mguruprasath416@users.noreply.github.com>
2025-07-27 18:07:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://phish-guard-orpin.vercel.app',
   credentials: true
 }));
 app.use(express.json());
